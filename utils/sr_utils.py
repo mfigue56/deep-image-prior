737102ac0dd37807ce503301ff610557ac656e56
mfigue56 <106602830+mfigue56@users.noreply.github.com>
2022-06-07 11:09:25 -0400

    Update sr_utils.py

--- utils/sr_utils.py
+++ utils/sr_utils.py
@@ -14,39 +14,7 @@ def put_in_center(img_np, target_size):
     
     return img_out
 
-#my work begins
-import numpy as np
-from numpy.fft import fftshift, ifftshift, fftn, ifftn
 
-def transform_image_to_kspace(img, dim=None, k_shape=None):
-    """ Computes the Fourier transform from image space to k-space space
-    along a given or all dimensions
-    :param img: image space data
-    :param dim: vector of dimensions to transform
-    :param k_shape: desired shape of output k-space data
-    :returns: data in k-space (along transformed dimensions)
-    """
-    if not dim:
-        dim = range(img.ndim)
-
-    k = fftshift(fftn(ifftshift(img, axes=dim), s=k_shape, axes=dim), axes=dim)
-    k /= np.sqrt(np.prod(np.take(img.shape, dim)))
-    return k
-
-def transform_kspace_to_image(k, dim=None, img_shape=None):
-    """ Computes the Fourier transform from k-space to image space
-    along a given or all dimensions
-    :param k: k-space data
-    :param dim: vector of dimensions to transform
-    :param img_shape: desired shape of output image
-    :returns: data in image space (along transformed dimensions)
-    """
-    if not dim:
-        dim = range(k.ndim)
-
-    img = fftshift(ifftn(ifftshift(k, axes=dim), s=img_shape, axes=dim), axes=dim)
-    img *= np.sqrt(np.prod(np.take(img.shape, dim)))
-    return img
 
 def load_LR_HR_imgs_sr(fname, imsize, factor, enforse_div32=None):
     '''Loads an image, resizes it, center crops and downscales.
@@ -58,28 +26,26 @@ def load_LR_HR_imgs_sr(fname, imsize, factor, enforse_div32=None):
         enforse_div32: if 'CROP' center crops an image, so that its dimensions are divisible by 32.
     '''
     img_orig_pil, img_orig_np = get_image(fname, -1)
-    img_orig_pilk = transform_image_to_kspace(img_orig_pil)  #change made by marcos
-    img_orig_npk = transform_image_to_kspace(img_orig_np)    #this one too
 
     if imsize != -1:
         img_orig_pil, img_orig_np = get_image(fname, imsize)
         
     # For comparison with GT
     if enforse_div32 == 'CROP':
-        new_size = (img_orig_pilk.size[0] - img_orig_pilk.size[0] % 32, 
-                    img_orig_pilk.size[1] - img_orig_pilk.size[1] % 32)
+        new_size = (img_orig_pil.size[0] - img_orig_pil.size[0] % 32, 
+                    img_orig_pil.size[1] - img_orig_pil.size[1] % 32)
 
         bbox = [
-                (img_orig_pilk.size[0] - new_size[0])/2, 
-                (img_orig_pilk.size[1] - new_size[1])/2,
-                (img_orig_pilk.size[0] + new_size[0])/2,
-                (img_orig_pilk.size[1] + new_size[1])/2,
+                (img_orig_pil.size[0] - new_size[0])/2, 
+                (img_orig_pil.size[1] - new_size[1])/2,
+                (img_orig_pil.size[0] + new_size[0])/2,
+                (img_orig_pil.size[1] + new_size[1])/2,
         ]
 
-        img_HR_pil = img_orig_pilk.crop(bbox)
+        img_HR_pil = img_orig_pil.crop(bbox)
         img_HR_np = pil_to_np(img_HR_pil)
     else:
-        img_HR_pil, img_HR_np = img_orig_pilk, img_orig_npk
+        img_HR_pil, img_HR_np = img_orig_pil, img_orig_np
         
     LR_size = [
                img_HR_pil.size[0] // factor, 
@@ -92,8 +58,8 @@ def load_LR_HR_imgs_sr(fname, imsize, factor, enforse_div32=None):
     print('HR and LR resolutions: %s, %s' % (str(img_HR_pil.size), str (img_LR_pil.size)))
 
     return {
-                'orig_pil': img_orig_pilk,
-                'orig_np':  img_orig_npk,
+                'orig_pil': img_orig_pil,
+                'orig_np':  img_orig_np,
                 'LR_pil':  img_LR_pil, 
                 'LR_np': img_LR_np,
                 'HR_pil':  img_HR_pil, 
