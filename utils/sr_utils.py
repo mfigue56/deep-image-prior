bfafb0919717d3cb7f2bc6f17b5c0326f9ac929e
mfigue56 <106602830+mfigue56@users.noreply.github.com>
2022-06-09 11:35:12 -0400

    Update sr_utils.py

--- utils/sr_utils.py
+++ utils/sr_utils.py
@@ -72,7 +72,7 @@ def load_LR_HR_imgs_sr(fname, imsize, factor, enforse_div32=None):
 
     #img_LR_pil = img_HR_pil.resize(LR_size, Image.ANTIALIAS)
     #img_LR_np = pil_to_np(img_LR_pil)
-    img_LR_pil = np_to_pil(img_LR_np)
+    
     
     ######Changes by Marcos#####
     ft = np.fft.fftshift(np.fft.fft2(img))
