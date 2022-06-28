3dfd866aba2d250597fdbad468c86f03c6830681
mfigue56 <106602830+mfigue56@users.noreply.github.com>
2022-06-28 09:39:32 -0400

    Update denoising_utils.py

--- utils/denoising_utils.py
+++ utils/denoising_utils.py
@@ -3,6 +3,7 @@ from .common_utils import *
 import numpy as np
 import scipy.io
 import matplotlib.image as mpimg 
+from PIL import Image, ImageSequence
         
 def get_noisy_image(path_to_image, img_np, sigma):
     """Adds Gaussian noise to an image.
@@ -12,19 +13,18 @@ def get_noisy_image(path_to_image, img_np, sigma):
         sigma: std of the noise
     """
 
-    #Marcos Additions
-    ##transform image to k-space (Fourier Transfrom)
-    #im = mpimg.imread(path_to_image)
-    #x = im[::2,::2,0]
-    #y = np.fft.fft2(x)
-    #clim = np.quantile(np.abs(y.reshape(-1)), [0.01,0.99])
-    #zero padding
-    #y = np.fft.fftshift(y)
-    #z = np.fft.ifft2(y, s=im.shape[:2]) #zero padded ifft
-    #clim = np.quantile(np.abs(z.reshape(-1)),[0.01,0.99])
+    #Marcos Code
+    im = Image.open(path_to_image)
+    img = im.convert('L')
     
-    #changed img_np to clim (undone)
-    img_noisy_np = np.clip(img_np + np.random.normal(scale=sigma, size=img_np.shape), 0, 1).astype(np.float32)
+    ft = np.fft.fftshift(np.fft.fft2(img, norm = 'forward'))
+    noise = np.random.normal(0, .1, ft.shape)
+    noisy_signal = ft + noise
+    ift = np.abs(np.fft.ifft2(noisy_signal, norm = 'forward')) #inverse transfrom
+    
+    img_noisy_np = ift
+
+    #img_noisy_np = np.clip(img_np + np.random.normal(scale=sigma, size=img_np.shape), 0, 1).astype(np.float32)
     img_noisy_pil = np_to_pil(img_noisy_np)
 
     return img_noisy_pil, img_noisy_np
